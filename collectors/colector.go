efb9a3e37a9d665e35746feca7f1cacfed862548
Tal Friedman Gamzon <talfridman4@gmail.com>
2024-03-04 21:39:40 +0200

    moved describe and collect logic to be generic in the BaseCollector

--- collectors/colector.go
+++ collectors/colector.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/talfridmen/lustre_exporter/collectortypes"
 )
 
 type Collector interface {
@@ -15,23 +16,44 @@ type Collector interface {
 
 // Collector represents the interface for a collector
 type BaseCollector struct {
-	name  string
-	level Level
+	name            string
+	level           Level
+	statsCollectors []collectortypes.StatsCollector
 }
 
-func (c BaseCollector) CollectBasicMetrics(ch chan<- prometheus.Metric) {
-	panic(fmt.Sprintf("CollectBasicMetrics is no implemented for collector %s", c.name))
-}
-func (c BaseCollector) CollectExtendedMetrics(ch chan<- prometheus.Metric) {
-	panic(fmt.Sprintf("CollectExtendedMetrics is no implemented for collector %s", c.name))
-}
-func (c BaseCollector) Describe(ch chan<- *prometheus.Desc) {
-	panic(fmt.Sprintf("Describe is no implemented for collector %s", c.name))
-}
+// func (c BaseCollector) CollectBasicMetrics(ch chan<- prometheus.Metric) {
+// 	panic(fmt.Sprintf("CollectBasicMetrics is no implemented for collector %s", c.name))
+// }
+// func (c BaseCollector) CollectExtendedMetrics(ch chan<- prometheus.Metric) {
+// 	panic(fmt.Sprintf("CollectExtendedMetrics is no implemented for collector %s", c.name))
+// }
+// func (c BaseCollector) Describe(ch chan<- *prometheus.Desc) {
+// 	panic(fmt.Sprintf("Describe is no implemented for collector %s", c.name))
+// }
 
 // GetLevel returns the level of operation for the collector
 func (c *BaseCollector) GetLevel() Level {
 	return c.level
+}
+
+func (c *BaseCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, statsCollector := range c.statsCollectors {
+		statsCollector.Describe(ch)
+	}
+}
+
+// CollectBasicMetrics collects basic metrics
+func (c *BaseCollector) CollectBasicMetrics(ch chan<- prometheus.Metric) {
+	for _, statsCollector := range c.statsCollectors {
+		statsCollector.CollectBasicMetrics(ch)
+	}
+}
+
+// CollectExtendedMetrics collects extended metrics
+func (c *BaseCollector) CollectExtendedMetrics(ch chan<- prometheus.Metric) {
+	for _, statsCollector := range c.statsCollectors {
+		statsCollector.CollectExtendedMetrics(ch)
+	}
 }
 
 // Level represents the operation level of a collector
--- collectors/mdt.go
+++ collectors/mdt.go
@@ -10,7 +10,6 @@ import (
 
 type MDTCollector struct {
 	BaseCollector
-	statsCollectors []collectortypes.StatsCollector
 }
 
 func NewMDTCollector(name string, level string) *MDTCollector {
@@ -18,43 +17,22 @@ func NewMDTCollector(name string, level string) *MDTCollector {
 		BaseCollector: BaseCollector{
 			name:  name,
 			level: getCollectorLevel(name, level),
+			statsCollectors: []collectortypes.StatsCollector{
+				*collectortypes.NewStatsCollector(
+					prometheus.NewDesc("lustre_mdt_stats_samples", "number of samples of metadata operations", []string{"path", "stat_type"}, nil),
+					prometheus.NewDesc("lustre_mdt_stats_sum", "number of samples of metadata operations", []string{"path", "stat_type", "units"}, nil),
+					prometheus.NewDesc("lustre_mdt_stats_sumsq", "number of samples of metadata operations", []string{"path", "stat_type", "units"}, nil),
+					[]string{fmt.Sprintf("%s/mdt/*/md_stats", consts.ProcfsBaseDir)},
+					[]string{},
+				),
+				*collectortypes.NewStatsCollector(
+					prometheus.NewDesc("lustre_mdt_export_stats_samples", "number of samples of metadata operations per export", []string{"path", "stat_type"}, nil),
+					prometheus.NewDesc("lustre_mdt_export_stats_sum", "number of samples of metadata operations per export", []string{"path", "stat_type", "units"}, nil),
+					prometheus.NewDesc("lustre_mdt_export_stats_sumsq", "number of samples of metadata operations per export", []string{"path", "stat_type", "units"}, nil),
+					[]string{},
+					[]string{fmt.Sprintf("%s/mdt/*/exports/*/stats", consts.ProcfsBaseDir)},
+				),
+			},
 		},
-
-		statsCollectors: []collectortypes.StatsCollector{
-			*collectortypes.NewStatsCollector(
-				prometheus.NewDesc("lustre_mdt_stats_samples", "number of samples of metadata operations", []string{"path", "stat_type"}, nil),
-				prometheus.NewDesc("lustre_mdt_stats_sum", "number of samples of metadata operations", []string{"path", "stat_type", "units"}, nil),
-				prometheus.NewDesc("lustre_mdt_stats_sumsq", "number of samples of metadata operations", []string{"path", "stat_type", "units"}, nil),
-				[]string{fmt.Sprintf("%s/mdt/*/md_stats", consts.ProcfsBaseDir)},
-				[]string{},
-			),
-			*collectortypes.NewStatsCollector(
-				prometheus.NewDesc("lustre_mdt_export_stats_samples", "number of samples of metadata operations per export", []string{"path", "stat_type"}, nil),
-				prometheus.NewDesc("lustre_mdt_export_stats_sum", "number of samples of metadata operations per export", []string{"path", "stat_type", "units"}, nil),
-				prometheus.NewDesc("lustre_mdt_export_stats_sumsq", "number of samples of metadata operations per export", []string{"path", "stat_type", "units"}, nil),
-				[]string{},
-				[]string{fmt.Sprintf("%s/mdt/*/exports/*/stats", consts.ProcfsBaseDir)},
-			),
-		},
-	}
-}
-
-func (c *MDTCollector) Describe(ch chan<- *prometheus.Desc) {
-	for _, statsCollector := range c.statsCollectors {
-		statsCollector.Describe(ch)
-	}
-}
-
-// CollectBasicMetrics collects basic metrics
-func (c *MDTCollector) CollectBasicMetrics(ch chan<- prometheus.Metric) {
-	for _, statsCollector := range c.statsCollectors {
-		statsCollector.CollectBasicMetrics(ch)
-	}
-}
-
-// CollectExtendedMetrics collects extended metrics
-func (c *MDTCollector) CollectExtendedMetrics(ch chan<- prometheus.Metric) {
-	for _, statsCollector := range c.statsCollectors {
-		statsCollector.CollectExtendedMetrics(ch)
 	}
 }
